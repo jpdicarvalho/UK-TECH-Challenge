a16b7fb9b60ffef3ad824b3baeb32203f23bcc73
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-04-01 12:55:36 -0300

    feat: GetStarted.jsx & react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GetStarted from './GetStarted';
+
 function App() {
 
   return (
     <>
-      <div>
-        hello word
-      </div>
+      <Router>
+        <Routes>
+
+            <Route path="/" element={<GetStarted />} />
       
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/GetStarted.jsx
@@ -0,0 +1,18 @@
+function GetStarted() {
+
+    return (
+      <>
+        <div>
+          <div className="section__background__img">background</div>
+         <div className="section__header">header</div>
+         <div className="section__featured__games">
+            <div className="box__games">box games</div>
+            <div className="box__games__selected">games selected</div>
+         </div>
+        </div>
+        
+      </>
+    )
+  }
+  
+  export default GetStarted

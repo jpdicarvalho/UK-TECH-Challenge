54db1018a614729e1c1fa846de886cbeef014fc2
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-04-01 13:35:15 -0300

    feat: header of GetStarted

--- src/GetStarted.jsx
+++ src/GetStarted.jsx
@@ -1,16 +1,37 @@
+
+import { useState } from "react"
+import Logo from "../src/assets/logo.png"
+
+import './GetStarted.css'
+
 function GetStarted() {
+    const [isBtnSelected, setIsBtnSelected] = useState(false)
 
     return (
       <>
-        <div>
-          <div className="section__background__img">background</div>
-         <div className="section__header">header</div>
+         <div className="section__background__img">background</div>
+         <div className="section__header">
+            <div className="box__logo">
+              <img src={Logo}  className="inner__logo" alt="" />
+            </div>
+            <div className="box__btns">
+              <button
+                className={`inner__btn ${isBtnSelected ? 'btn__active':''}`}
+                onClick={()=> setIsBtnSelected(true)}>
+                  Acessar conta
+              </button>
+
+              <button 
+                className={`inner__btn ${isBtnSelected ? '':'btn__active'}`}
+                onClick={()=> setIsBtnSelected(false)}>
+                  Cadastre-se
+                </button>
+            </div>
+         </div>
          <div className="section__featured__games">
             <div className="box__games">box games</div>
             <div className="box__games__selected">games selected</div>
-         </div>
-        </div>
-        
+         </div>        
       </>
     )
   }

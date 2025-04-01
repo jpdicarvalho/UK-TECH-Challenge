9b5721289ac045735c928a7962feaaa3670157d2
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-04-01 18:45:05 -0300

    feat: card featured games

--- src/GetStarted.jsx
+++ src/GetStarted.jsx
@@ -1,15 +1,21 @@
 
 import { useState } from "react"
+
 import Logo from "../src/assets/logo.png"
+import CrashImg from "../src/assets/crash-img.png"
 
 import './GetStarted.css'
 
+
 function GetStarted() {
     const [isBtnSelected, setIsBtnSelected] = useState(false)
-
+    
+    //<img src={CrashImg} alt="" />
+    
     return (
       <>
-         <div className="section__background__img">background</div>
+         <div className="section__background__img">
+         </div>
          <div className="section__header">
             <div className="box__logo">
               <img src={Logo}  className="inner__logo" alt="" />
@@ -29,7 +35,23 @@ function GetStarted() {
             </div>
          </div>
          <div className="section__featured__games">
-            <div className="box__games">box games</div>
+            <div className="box__games">
+              <div
+                className="inner__games"
+                onClick={()=> setIsBtnSelected(false)}>
+                <img src={CrashImg}  className="inner__img__game" alt="" />
+                <p className="name__game">Crash Team Racing Nitro</p>
+              </div>
+
+              <div className="inner__games">
+                <img src={CrashImg}  className="inner__img__game" alt="" />
+                <p className="name__game">Crash Team Racing Nitro</p>
+              </div>
+
+              <div className="inner__games">
+                <img src={CrashImg}  className="inner__img__game" alt="" />
+              </div>
+            </div>
             <div className="box__games__selected">games selected</div>
          </div>        
       </>

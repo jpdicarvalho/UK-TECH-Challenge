51433d0767114f899fc09ebdcccc53ab39bb6618
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-04-01 20:39:59 -0300

    feat: cards of games featured

--- src/GetStarted.jsx
+++ src/GetStarted.jsx
@@ -3,19 +3,60 @@ import { useState } from "react"
 
 import Logo from "../src/assets/logo.png"
 import CrashImg from "../src/assets/crash-img.png"
+import MinecraftImg from "../src/assets/minecraft-img.jpg"
+import GenshinImg from "../src/assets/genshin-img.jpg"
+
 
 import './GetStarted.css'
 
+const featuredGames = [
+  {
+    id: 1,
+    name: 'Crash Team Racing Nitro',
+    image: CrashImg,
+  },
+  {
+    id: 2,
+    name: 'Minecraft',
+    image: MinecraftImg,
+  },
+  {
+    id: 3,
+    name: 'Genshin Impact',
+    image: GenshinImg,
+  },
+
+]
 
 function GetStarted() {
     const [isBtnSelected, setIsBtnSelected] = useState(false)
-    
-    //<img src={CrashImg} alt="" />
-    
+    const [gameSelected, setGameSelected] = useState([0]);
+
+    const toggleItem = (itemId) => {
+      if (gameSelected === itemId) {
+        setGameSelected(null); 
+      } else {
+        setGameSelected(itemId);
+      }
+    };
+
+    const selectedGame = featuredGames.find(game => game.id === gameSelected);
+
+
     return (
       <>
-         <div className="section__background__img">
-         </div>
+          <div className="section__background__img">
+            {selectedGame && (
+              <>
+                <img
+                  src={selectedGame.image}
+                  className="inner__background__img__game__selected"
+                  alt={selectedGame.name}
+                />
+                <div className="background-overlay" />
+              </>
+            )}
+          </div>
          <div className="section__header">
             <div className="box__logo">
               <img src={Logo}  className="inner__logo" alt="" />
@@ -36,24 +77,23 @@ function GetStarted() {
          </div>
          <div className="section__featured__games">
             <div className="box__games">
-              <div
-                className="inner__games"
-                onClick={()=> setIsBtnSelected(false)}>
-                <img src={CrashImg}  className="inner__img__game" alt="" />
-                <p className="name__game">Crash Team Racing Nitro</p>
-              </div>
-
-              <div className="inner__games">
-                <img src={CrashImg}  className="inner__img__game" alt="" />
-                <p className="name__game">Crash Team Racing Nitro</p>
-              </div>
-
-              <div className="inner__games">
-                <img src={CrashImg}  className="inner__img__game" alt="" />
-              </div>
+              {featuredGames.map((game, index) => (
+                <div
+                  key={index}
+                  className={`game-card ${gameSelected === game.id ? 'game-card--active' : ''}`}
+                  onClick={() => toggleItem(game.id)}
+                >
+                  <img src={game.image} className="game-image" alt={game.name} />
+                  <div className="overlay">
+                      <h3 className={`game-title ${gameSelected === game.id ? 'game-title--active' : ''}`}>{game.name}</h3>
+                  </div>
+                </div>
+              ))}
             </div>
-            <div className="box__games__selected">games selected</div>
-         </div>        
+            <div className="box__games__selected">
+              games selected
+            </div>
+          </div>        
       </>
     )
   }

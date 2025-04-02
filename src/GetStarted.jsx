71ffbeecad3b02a1b9feb5dcecfe50414503d8b1
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-04-02 12:25:40 -0300

    feat: section most payed games & reward coins

--- src/GetStarted.jsx
+++ src/GetStarted.jsx
@@ -1,14 +1,25 @@
 
 import { useState } from "react"
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+
 import Logo from "../src/assets/logo.png"
 import LabelGame from "../src/assets/label-game.png"
 import CrashImg from "../src/assets/crash-img.png"
 import MinecraftImg from "../src/assets/minecraft-img.jpg"
 import GenshinImg from "../src/assets/genshin-img.jpg"
+import NineImg from "../src/assets/nine-img.png"
+import GenshinImpactImg from "../src/assets/genshin-capa-img.png"
+import AliensImg from "../src/assets/aliens-img.png"
+import AgeOfImpiresIImg from "../src/assets/age-of-empires-img.png"
+import BannerImg from "../src/assets/banner-image.png"
+import CoinImg from "../src/assets/coin-img.png"
 
 import { IoIosHeartEmpty } from "react-icons/io";
 import { IoPlay } from "react-icons/io5";
+import { MdArrowRightAlt } from "react-icons/md";
 
 
 import './GetStarted.css'
@@ -31,6 +42,39 @@ const featuredGames = [
     name: 'Genshin Impact',
     image: GenshinImg,
     label: 'RPG de ação e mundo aberto'
+  }
+]
+
+const mostPlayedGames = [
+  {
+    id: 1,
+    name: 'Nine  Sols',
+    image: NineImg,
+    users: 224
+  },
+  {
+    id: 2,
+    name: 'Genish Impact',
+    image: GenshinImpactImg,
+    users: 224
+  },
+  {
+    id: 3,
+    name: 'Aliens Dark Descent',
+    image: AliensImg,
+    users: 224
+  },
+  {
+    id: 4,
+    name: 'Age of Empires II',
+    image: AgeOfImpiresIImg,
+    users: 224
+  },
+  {
+    id: 5,
+    name: 'Minecraft',
+    image: MinecraftImg,
+    users: 224
   }
 ]
 
@@ -119,10 +163,63 @@ function GetStarted() {
           </div>
 
           <div className="section__most__played__games">
-              <h3>Jogos mais jogados</h3>
-              <div className="container__games">
+            <div className="background__wrapper">
+              <img src={NineImg} className="img__background" alt="" />
+              <div className="background__overlay" />
+            </div>
 
+            <div className="header__most__played__games">
+              <h3 className="tittle__most__payed__games">Jogos mais jogados</h3>
+              <div className="box__see__more center">
+                <p className="text__see__more">VER MAIS</p>
+                <MdArrowRightAlt />
               </div>
+            </div>
+
+            <div className="container__most__played__games">
+              <Swiper
+                modules={[Autoplay]}
+                spaceBetween={16}
+                slidesPerView={3}
+                loop={true}
+                speed={7000}
+                autoplay={{
+                  delay: 0,
+                  disableOnInteraction: false,
+                }}
+                breakpoints={{
+                  640: { slidesPerView: 2 },
+                  768: { slidesPerView: 3 },
+                  1024: { slidesPerView: 4 },
+                }}
+              >
+                {mostPlayedGames.map((game, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="card__most__payed__game">
+                      <img src={game.image} className="img__cover__game" alt={game.name} />
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+
+          <div className="section__reward__coins">
+            <div className="box__reward__coins">
+              <div className="box__img__coin center">
+                <img src={CoinImg} alt="" />
+              </div>
+              <div className="box__text__reward__coins">
+                <h3 className="text__reward__coins">
+                  Ganhe <span className="highlight-number">100</span> moedas grátis!
+                </h3>
+                <p style={{fontSize: '12px'}}>ao fazer seu cadastro</p>
+                <button className="btn__reward__coins">Cadastre-se agora <MdArrowRightAlt /></button>
+              </div>
+            </div>
+            <div className="box__banner__img">
+              <img src={BannerImg} className="inner__banner__img" alt="" />
+            </div>
           </div>
       </>
     )

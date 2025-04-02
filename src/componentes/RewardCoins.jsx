2202458aae7664ed46281b807e0d32dd99fe0a88
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-04-02 15:49:13 -0300

    Refactor: RewardCoins.jsx

---
+++ src/componentes/RewardCoins.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import CoinImg from "../assets/coin-img.png";
+import BannerImg from "../assets/banner-image.png";
+import { MdArrowRightAlt } from "react-icons/md";
+
+function RewardCoins() {
+  return (
+    <div className="section__reward__coins">
+      <div className="box__reward__coins">
+        <div className="box__img__coin center">
+          <img src={CoinImg} alt="Moeda" />
+        </div>
+        <div className="box__text__reward__coins">
+          <h3 className="text__reward__coins">
+            Ganhe <span className="highlight-number">100</span> moedas grátis!
+          </h3>
+          <p style={{ fontSize: "12px" }}>ao fazer seu cadastro</p>
+          <button className="btn__reward__coins">
+            Cadastre-se agora <MdArrowRightAlt />
+          </button>
+        </div>
+      </div>
+
+      <div className="box__banner__img">
+        <img src={BannerImg} className="inner__banner__img" alt="Banner promocional" />
+      </div>
+    </div>
+  );
+}
+
+export default RewardCoins;
